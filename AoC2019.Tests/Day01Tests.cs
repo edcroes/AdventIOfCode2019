cf8a0da40f1c877265c44341813b98fd4f465bcb
Edwin Croes <e.croes@rubicon.nl>
2021-01-07 07:42:19 +0100

    Day 1

---
+++ AoC2019.Tests/Day01Tests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC2019.Tests
+{
+    [TestClass]
+    public class Day01Tests : DayTestsBase<Day01.Day01>
+    {
+        public Day01Tests() : base("3502510", "5250885") { }
+    }
+}
---
+++ AoC2019/Day01/Day01.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AoC2019.Day01
+{
+    public class Day01 : IMDay
+    {
+        private readonly int[] _modules;
+
+        public Day01()
+        {
+            _modules = File.ReadAllLines("Day01\\input.txt")
+                .Where(l => !string.IsNullOrEmpty(l))
+                .Select(l => int.Parse(l))
+                .ToArray();
+        }
+
+        public string GetAnswerPart1()
+        {
+            var fuelConsumption = _modules
+                .Select(m => GetFuelConsumption(m))
+                .Sum();
+
+            return fuelConsumption.ToString();
+        }
+
+        public string GetAnswerPart2()
+        {
+            var fuelConsumption = _modules
+                .Select(m => GetTotalFuelConsumption(m))
+                .Sum();
+
+            return fuelConsumption.ToString();
+        }
+
+        private static int GetFuelConsumption(int module) => module / 3 - 2;
+
+        private static int GetTotalFuelConsumption(int fuelUser)
+        {
+            var fuelRequired = GetFuelConsumption(fuelUser);
+            return fuelRequired > 0
+                ? fuelRequired + GetTotalFuelConsumption(fuelRequired)
+                : 0;
+        }
+    }
+}
