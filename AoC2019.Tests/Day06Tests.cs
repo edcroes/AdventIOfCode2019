6e14d4f479b0018c1f9941534ec651b68b2276b2
Edwin Croes <ed_croes@hotmail.com>
2021-01-08 14:46:58 +0100

    Tests day 6

---
+++ AoC2019.Tests/Day06Tests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC2019.Tests
+{
+    [TestClass]
+    public class Day06Tests : DayTestsBase<Day06.Day06>
+    {
+        public Day06Tests() : base("344238", "436") { }
+    }
+}
